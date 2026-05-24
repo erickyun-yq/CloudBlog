e033e0c972e5b76235e88c670a55901e07101500
erickyum <2410916019@qq.com>
2026-05-24 19:41:13 +0800

    完善配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  routeRules: {
+    //路由规则配置，详情见https://nuxtjs.org.cn/docs/4.x/guide/concepts/rendering#route-rules
+    '/': { prerender: true },
+    '/products': { swr: true },
+    '/blog': { isr: true },
+    '/admin/**': { ssr: false },
+    '/api/**': { cors: true },
+    '/old-page': { redirect: '/new-page' }, 
+  },   
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
@@ -9,6 +18,26 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
   ],
+  content: {
+
+  },
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: true,
+    }
+  },                 
+  fonts: {
+    families: [
+      { name: 'Alimama DongFangDaKai', global: true, provider: 'local', src: '/fonts/AlimamaDongFangDaKai-Regular.ttf' },
+      { name: 'Alimama AgileVF', global: true, provider: 'local', src: '/fonts/AlimamaAgileVF-Thin.ttf' },
+    ],
+  },
+  antd: {
+  },
+  pinia: {
+    storesDirs: []
+  },
   devtools: { enabled: true },
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2026-05-28',
 })
