f08c3d0a2586995096df9b9139ce26342ba8cf12
erickyum <2410916019@qq.com>
2026-05-25 01:04:07 +0800

    完善配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  routeRules: {
-    //路由规则配置，详情见https://nuxtjs.org.cn/docs/4.x/guide/concepts/rendering#route-rules
-    '/': { prerender: true },
-    '/products': { swr: true },
-    '/blog': { isr: true },
-    '/admin/**': { ssr: false },
-    '/api/**': { cors: true },
-    '/old-page': { redirect: '/new-page' }, 
-  },   
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
@@ -18,26 +9,38 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
   ],
+  devtools: { enabled: true },
   content: {
-
+    // https://content.nuxtjs.org/configuration
+  },
+  routeRules: {
+    // 路由规则配置，详情见https://nuxtjs.org.cn/docs/4.x/guide/concepts/rendering#route-rules
+    '/': { prerender: true },
+    '/products': { swr: true },
+    '/blog': { isr: true },
+    '/admin/**': { ssr: false },
+    '/api/**': { cors: true },
+    '/old-page': { redirect: '/new-page' },
+  },
+  compatibilityDate: '2026-05-28',
+  antd: {
   },
   eslint: {
-    checker: true,
+    checker: {
+      eslintPath: 'eslint',
+    },
     config: {
       stylistic: true,
-    }
-  },                 
+    },
+  },
   fonts: {
+    provider: 'local',
     families: [
       { name: 'Alimama DongFangDaKai', global: true, provider: 'local', src: '/fonts/AlimamaDongFangDaKai-Regular.ttf' },
       { name: 'Alimama AgileVF', global: true, provider: 'local', src: '/fonts/AlimamaAgileVF-Thin.ttf' },
     ],
   },
-  antd: {
-  },
   pinia: {
-    storesDirs: []
+    storesDirs: [],
   },
-  devtools: { enabled: true },
-  compatibilityDate: '2026-05-28',
 })
